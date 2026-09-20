b3232205dd02db5c81e5a0df8a3aae95be8c09a4
Austin McChord <austin@mcchord.net>
2026-09-19 23:13:20 -0400

    Enable idle ground driving with safe balance handoff

--- scripts/check_balance_candidate.sh
+++ scripts/check_balance_candidate.sh
@@ -19,6 +19,8 @@ clang++ -std=c++17 -Wall -Wextra -Werror -Itests/stubs -Isrc tests/test_crsf_nat
 output/test_crsf_native
 clang++ -std=c++17 -Wall -Wextra -Werror -Wno-unused-parameter -Itests/stubs -Isrc tests/test_motor_feedback.cpp src/robstride.cpp src/motor_manager.cpp -o output/test_motor_feedback
 output/test_motor_feedback
+clang++ -std=c++17 -Wall -Wextra -Werror -Wno-unused-parameter -Itests/stubs -Isrc tests/test_ground_drive.cpp src/drive_controller.cpp src/robstride.cpp src/motor_manager.cpp -o output/test_ground_drive
+output/test_ground_drive
 clang++ -std=c++17 -Wall -Wextra -Werror -pthread -Isrc tests/test_network_safety.cpp -o output/test_network_safety
 output/test_network_safety
 "$PYTHON_BIN" -m unittest discover -s tests -v
